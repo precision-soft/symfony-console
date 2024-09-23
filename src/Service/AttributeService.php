f32102db1baeab2a6853171bb00cde6bf23dce2e
Adrian Jeledintan <adrian.jeledintan@precision-soft.com>
2024-09-23 14:13:47 +0300

    \PrecisionSoft\Symfony\Console\Service\AttributeService

---
+++ src/Service/AttributeService.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) Precision Soft
+ */
+
+namespace PrecisionSoft\Symfony\Console\Service;
+
+use PrecisionSoft\Symfony\Console\Exception\Exception;
+use ReflectionClass;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+class AttributeService
+{
+    public static function getCommandName(string $commandClass): string
+    {
+        $reflectionClass = new ReflectionClass($commandClass);
+        $attributes = $reflectionClass->getAttributes(AsCommand::class);
+
+        foreach ($attributes as $attribute) {
+            $instance = $attribute->newInstance();
+
+            return $instance->name;
+        }
+
+        throw new Exception(
+            sprintf('could not compute the name for `%s`', $commandClass)
+        );
+    }
+}
